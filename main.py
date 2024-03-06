e01934d737709aca44cbea7a367a37c6cb713740
John Doe <johndoe@example.com>
2024-03-06 12:23:03 +0700

    edit1

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ app = FastAPI()
 WriteARead = Controller()
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
 
 now = datetime.now()
 
