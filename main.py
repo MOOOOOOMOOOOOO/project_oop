118cc81721f9153322970ee4bf6097b1ddc8ccf6
John Doe <johndoe@example.com>
2024-03-06 19:33:58 +0700

    'k'k'

--- main.py
+++ main.py
@@ -81,26 +81,7 @@ Mo.add_golden_coin(888)
 Mo.add_book_shelf_list(Book1)
 Mo.add_book_shelf_list(Book2)
 #----------------------------------fastapi----------------------------------
-class dto_sign_up(BaseModel):
-    username:str
-    password:str
-    birth_date: str
-    role: str
 
-class dto_create_report(BaseModel):
-    username : str
-    report_type : str
-    content : str
-
-class dto_add_pseudonym(BaseModel):
-    username : str
-    new_pseudonym : str
-
-class dto_buy_chapter(BaseModel):
-    username :str
-    chapter_id : str
-
-#--------------------------------------------------------------------------------------------------------------
 
 @app.get("/")
 def FirstPage():
@@ -123,31 +104,50 @@ def ShowSilverCoins(username:str):
      user = WriteARead.get_user_by_username(username)
      return {"Silver_Coin" :user.show_silver_coin_list()}
 
+class dto_sign_up(BaseModel):
+    username:str
+    password:str
+    birth_date: str
+    role: str
+
 @app.post("/sign_up", tags=['sign up/sign in'])
 def SignUp(dto : dto_sign_up):
      return {"Sign Up" : WriteARead.sign_up(dto.username, dto.password, dto.birth_date)}
+
+class dto_create_report(BaseModel):
+    username : str
+    report_type : str
+    content : str
 
 @app.post("/create_report", tags=['report'])
 def CreateReport(dto : dto_create_report):
      return {"Create Report" : WriteARead.create_report(dto.username, dto.report_type, dto.content)}
 
+class dto_upgrade_to_writer(BaseModel):
+    username:str
+
 @app.put("/upgrade_to_writer", tags=['upgrade'])
-def upgrade_to_writer(username: str):
-    user = WriteARead.get_user_by_username(username)
+def upgrade_to_writer(dto : dto_upgrade_to_writer):
+    user = WriteARead.get_user_by_username(dto.username)
     if isinstance(user, Writer):
         return {"message": "User is already a writer."}
     else:
         WriteARead.add_writer(user)
         return {"message": "User upgraded to writer successfully!!!."}
-
-@app.get("/My Page", tags=['user'])
-def ShowMyPage(username:str):
-     return {"My Page" : WriteARead.show_my_page(username)}
+    
+class dto_edit_introduction(BaseModel):
+     username : str
+     text : str
+     
+@app.put("/my_page/edit_introduction", tags=["user"])
+def EditIntroduction(dto : dto_edit_introduction):
+     user = WriteARead.get_user_by_username(dto.username)
+     return {"Edit Introduction" : user.edit_introduction(dto.text)}
 
 @app.put("/My Page/Edit introduction", tags=["user"])
-def EditIntroduction(username:str, text:str):
-     user = WriteARead.get_user_by_username(username)
-     return {"Edit Introduction" : user.edit_introduction(text)}
+def EditIntroduction(dto : dto_edit_introduction):
+     user = WriteARead.get_user_by_username(dto.username)
+     return {"Edit Introduction" : user.edit_introduction(dto.text)}
 
 @app.get("/My Profile", tags=['user'])
 def ShowMyProfile(username:str):
@@ -172,9 +172,18 @@ def ShowChapterTransaction(username:str):
      if WriteARead.is_user_not_found(user): return user
      return {"Chapter Transaction" : user.show_chapter_transaction()}
 
-@app.put("/My Profile/change_password", tags=['user'])
-def ChangePassword(username:str,old_password:str, new_password:str):
-     return {"Change Password" : WriteARead.change_password(username, old_password, new_password)}
+class dto_change_password(BaseModel):
+     username : str
+     old_password :str
+     new_password : str
+
+@app.put("/my_profile/change_password", tags=['user'])
+def ChangePassword(dto : dto_change_password):
+     return {"Change Password" : WriteARead.change_password(dto.username, dto.old_password, dto.new_password)}
+
+class dto_add_pseudonym(BaseModel):
+    username : str
+    new_pseudonym : str
 
 @app.post("/my_profile/psedonym", tags=['user'])
 def  AddPseudonym(dto : dto_add_pseudonym):
@@ -183,6 +192,10 @@ def  AddPseudonym(dto : dto_add_pseudonym):
 @app.get("/My Reading", tags=['user'])
 def ShowMyReading(username:str):
      return {"My Reading" : WriteARead.show_my_reading(username)}
+
+class dto_buy_chapter(BaseModel):
+    username :str
+    chapter_id : str
 
 @app.post("/buy_chapter", tags=['chapter'])
 def BuyChapter(dto : dto_buy_chapter):
